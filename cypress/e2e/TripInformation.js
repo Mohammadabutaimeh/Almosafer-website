8b8d15685bbf1e7b4b65d09eaa305f5652f2f1c6
Mohammadabutaimeh <hamota741994@Gmail.com>
2023-09-12 21:35:54 +0300

    add trip information file.js to check the trip information tabs

---
+++ cypress/e2e/TripInformation.js
@@ -0,0 +1,36 @@
+/// <reference types= "cypress" />
+
+describe('This is to check Trip information', () => {
+    it('this is to check the departure date is set to be today date +1', () => {
+        cy.visit("https://www.almosafer.com/ar");
+        cy.get('.cta__saudi').click();
+        cy.wait(2000)
+        let tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        let day = String(tomorrow.getDate());
+        let month = tomorrow.toLocaleDateString('ar-AE', { month: 'long' });
+        let WeekDay = tomorrow.toLocaleDateString('ar-AE', { weekday: 'long' });
+        cy.get('[data-testid="FlightSearchBox__FromDateButton"]').should('include.text', `${month}${day} ${WeekDay}`);
+
+    })
+    it('this is to check the return date is set to be today date +2', () => {
+        cy.visit("https://www.almosafer.com/ar");
+        cy.get('.cta__saudi').click();
+        cy.wait(2000)
+        let tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 2);
+        let day = String(tomorrow.getDate());
+        let month = tomorrow.toLocaleDateString('ar-AE', { month: 'long' });
+        let WeekDay = tomorrow.toLocaleDateString('ar-AE', { weekday: 'long' });
+        cy.get('[data-testid="FlightSearchBox__ToDateButton"]').should('include.text', `${month}${day}${WeekDay}`);
+
+    })
+    it('this is to check the flight class is set to be economy by default', () => {
+        cy.visit("https://www.almosafer.com/ar");
+        cy.get('.cta__saudi').click();
+        cy.wait(2000)
+        cy.get('.sc-jWxkHr').should('contain.text', 'السياحية');
+    })
+})
+
+
